896ed858d81eeeaf445fb51f01a2b95053baa366
Matt Gallucci <mattgallucci97@gmail.com>
2023-12-26 14:01:44 -0800

    path config of media files, video generation, clean up

--- Generator/Generator.py
+++ Generator/Generator.py
@@ -1,7 +1,12 @@
 import os
+import subprocess
 from playwright.sync_api import sync_playwright
 
 class Generator():
+
+  template_path = os.path.abspath("page.html")
+  screenshot_path = os.path.abspath("screenshot.png")
+  audio_path = os.path.abspath("audio.mp3")
 
   def __init__(self):
     print('   ✅   generator created')
@@ -19,7 +24,7 @@ class Generator():
         </head>
         <body>
           <h1 class="title">{title} - {artist}</h1>
-          <img src="./artwork.png" class="artwork">
+          <img src="artwork.png" class="artwork">
           <h1 class="site-url">sweatshopbeats.com</h1>
         </body>
         <style>
@@ -68,15 +73,29 @@ class Generator():
       try:
         browser = p.chromium.launch()
         page = browser.new_page(viewport={"width": 1600, "height": 900})
-        template_path = os.path.abspath("./page.html")
-        page.goto('file://' + template_path)
-        page.screenshot(path="screenshot.png", full_page=True)
+        
+        page.goto('file://' + self.template_path)
+        page.screenshot(path=self.screenshot_path, full_page=True)
         print('   ✅   screenshot saved')
       except Exception as e:
         print(e)
 
 
+  def make_video(self):
+    try:
+      os.system(f"ffmpeg -loop 1 -i {self.screenshot_path} -i {self.audio_path} -r 1 -c:v libx264 -preset slow -tune stillimage -crf 18 -c:a copy -shortest -s 1280x720 out.mp4")
+      print('   ✅   video generated')
+    except Exception as e:
+      print('   ❌   video generation failed :(')
+      print(e)
+
+
   def clean_up(self):
-    os.remove('artwork.png')
-    os.remove('audio.mp3')
-    # os.remove('screenshot.png')
+    try:
+      os.remove('artwork.png')
+      os.remove('audio.mp3')
+      os.remove('page.html')
+      print('   ✅ cleanup done')
+    except Exception as e:
+      print('   ❌ cleanup failed')
+      print(e)
