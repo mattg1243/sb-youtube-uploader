bae44bd5ba3a691237722c906c74e7bf69f987f3
Matt Gallucci <mattgallucci97@gmail.com>
2023-12-28 12:07:06 -0800

    script work, gitignore

--- main.py
+++ main.py
@@ -6,51 +6,60 @@ import pika
 
 from Consumer.Consumer import Consumer
 from Generator.Generator import Generator
+from Uploader.Uploader import Uploader
 
 load_dotenv()
 MQ_URL = os.getenv('MQ_URL')
 MQ_PW = os.getenv('MQ_PW')
 
+
 def main():
 
-  # connect to queue
-  conn = pika.BlockingConnection(pika.URLParameters(MQ_URL))
+    # connect to queue
+    conn = pika.BlockingConnection(pika.URLParameters(MQ_URL))
 
-  c = Consumer(conn)
-  g = Generator()
+    c = Consumer(conn)
+    g = Generator()
+    u = Uploader()
 
-  # check queue for beats to upload
-  (beat, delivery_tag) = c.get_msg_from_queue()
-  if beat == None:
-    print('no beats in the queue for today, Ill check again tomorrow')
+    # check queue for beats to upload
+    (beat, delivery_tag) = c.get_msg_from_queue()
+    if beat == None:
+        print('no beats in the queue for today, Ill check again tomorrow')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
+    # if theres a beat in the queue, fetch the audio and artwork
     try:
-      sys.exit(0)
-    except SystemExit:
-      os._exit(0)
-
-  # if theres a beat in the queue, fetch the audio and artwork 
-  try:
-    urllib.request.urlretrieve(beat['img_link'], os.path.abspath('artwork.png'))
-    urllib.request.urlretrieve(beat['audio_link'], os.path.abspath('audio.mp3'))
-    # generate the html and save a screenshot
-    g.write_html(beat['title'], beat['artist'])
-    g.get_screenshot()
-    g.make_video()
-    print('   ✅   all done!')
-    # send ack to queue
-    c.send_ack(delivery_tag)
-  except Exception as e:
-    print(e)
+        urllib.request.urlretrieve(
+            beat['img_link'], os.path.abspath('artwork.png'))
+        urllib.request.urlretrieve(
+            beat['audio_link'], os.path.abspath('audio.mp3'))
+        # generate the html and save a screenshot
+        g.write_html(beat['title'], beat['artist'])
+        g.get_screenshot()
+        g.make_video()
+        print(beat)
+        u.upload_video(beat['title'], beat['artist'],
+                       beat['artist_id'], beat['beat_id'])
+        print('   ✅   all done!')
+        # send ack to queue
+        c.send_ack(delivery_tag)
+        g.clean_up()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
-  try :
-    main()
-  except KeyboardInterrupt:
-    print('closing')
-  except Exception as e:
-    print(e)
     try:
-      sys.exit(0)
-    except SystemExit:
-      os._exit(0)
+        main()
+    except KeyboardInterrupt:
+        print('closing')
+    except Exception as e:
+        print(e)
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
